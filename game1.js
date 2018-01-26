db35b7fda946124ef99d98db73122bb588956b49
User-PC\User <aliciamarie1112@yahoo.com>
2018-01-25 21:58:20 -0500

    updated code

--- game1.js
+++ game1.js
@@ -11,24 +11,24 @@ var grantscore=0;
 while (userscore < 3 && grantscore < 1) {
   var playerdamage = Math.floor((Math.random() * 2) + 1);
   var grantdamage = Math.floor((Math.random() * 2) + 1);
-  if (grantHealthPoints > 0 || userHealthPoints > 0) {
+  if (grantHealthPoints > 0 && userHealthPoints > 0) {
   userHealthPoints = userHealthPoints-playerdamage;
   grantHealthPoints = grantHealthPoints-grantdamage;
-  console.log("Grant has " + grantHealthPoints + "left.");
-  console.log("User has" + userHealthPoints + "left");
+  console.log("Grant has " + grantHealthPoints + " left.");
+  console.log("User has " + userHealthPoints + " left");
 }
-  else if(grantHealthPoints < 0) {
-    console.log("defeated");
+  else if(grantHealthPoints <= 0) {
+    console.log("Grant Defeated");
     userscore++;
     grantHealthPoints = 10;
   }
-  else if (userHealthPoints < 0) {
+  else if (userHealthPoints <= 0) {
     console.log("Grant wins!");
     grantscore++;
   }
 }
-if (userscore===3) {
+if (userscore==3) {
   console.log ("The winner is " + character);
-} else if (grantscore===1) {
+} else if (grantscore==1) {
   console.log("The winner is Granty boy.");
 }
